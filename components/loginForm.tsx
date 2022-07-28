6f1b88211c4dd5d96eb8eaa83ebc6ced7876934c
brianwachira <brianwachira7@gmail.com>
2022-07-28 18:02:48 +0300

    [feat] : login form styles

--- components/loginForm.tsx
+++ components/loginForm.tsx
@@ -1,35 +1,77 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import theme from '../styles/themes';
 import Button from './shared-ui/button';
 import FormikTextInput from './shared-ui/formikTextInput';
+import Text from './shared-ui/text';
+
+const styles = StyleSheet.create({
+  onBoardingInput: {
+    color: theme.colors.black,
+    borderBottomColor: theme.colors.black,
+    borderBottomWidth: 1,
+    height: 40,
+  },
+  label: {
+    textAlign: 'center',
+    marginTop: 14,
+    marginBottom: 10,
+  },
+  formContainer: {
+    flex: 0.8,
+    justifyContent: 'center',
+    marginHorizontal: 50,
+  },
+  inputContainer: {
+    marginBottom: 40,
+  },
+  buttonContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+});
 
 const LoginForm = ({onSubmit}: {onSubmit: any}) => {
   return (
     <>
-      <View>
-        {/* email */}
-        <Text>Email Address</Text>
-        <FormikTextInput
-          name="email"
-          textContentType="emailAddress"
-          autoComplete="email"
-        />
-
+      <View style={styles.formContainer}>
+        <View style={styles.inputContainer}>
+          {/* email */}
+          <Text textType="labelInput">Email Address</Text>
+          <FormikTextInput
+            name="email"
+            textContentType="emailAddress"
+            autoComplete="email"
+            style={styles.onBoardingInput}
+          />
+        </View>
         {/* password */}
-        <Text>Password</Text>
-        <FormikTextInput
-          name="password"
-          textContentType="password"
-          autoComplete="password"
+        <View style={styles.inputContainer}>
+          <Text textType="labelInput">Password</Text>
+          <FormikTextInput
+            name="password"
+            textContentType="password"
+            autoComplete="password"
+            style={styles.onBoardingInput}
+            secureTextEntry
+          />
+        </View>
+
+        {/* forgot password link */}
+        <Text color="primary" textType="labelLink">
+          Forgot Password{' '}
+        </Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          onPress={onSubmit}
+          title="Login"
+          buttonType="orange"
+          textType="labelButtonOrange"
+          accessibilityLabel="Login"
         />
       </View>
-      <Button
-        onPress={onSubmit}
-        title="Login"
-        buttonType="orange"
-        textType="labelButtonOrange"
-        accessibilityLabel="Login"
-      />
     </>
   );
 };
