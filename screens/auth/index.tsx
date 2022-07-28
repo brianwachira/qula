ee4c5b1090ad966e48e752b6958612989fa2a7e8
Brian Wachira <brianwachira7@gmail.com>
2022-07-28 22:40:31 +0300

    [refactor]: define styles separately from tab

--- screens/auth/index.tsx
+++ screens/auth/index.tsx
@@ -9,19 +9,46 @@ import {
   useWindowDimensions,
   Dimensions,
   Image,
+  StyleSheet,
 } from 'react-native';
 import {TabBar, TabView} from 'react-native-tab-view';
 import theme from '../../styles/themes';
 import {RootStackParamList} from '../../types';
 import Login from './login';
+import Signup from './signup';
 
-const SecondRoute = () => {
-  return (
-    <View style={{flex: 1, backgroundColor: '#F2F2F2'}}>
-      <Text>Sign Up</Text>
-    </View>
-  );
-};
+const styles = StyleSheet.create({
+  tabContainer: {
+    backgroundColor: theme.colors.tab,
+  },
+  tabBarStyle: {
+    height: Dimensions.get('window').height / 3,
+    borderBottomRightRadius: theme.borderRadius.button,
+    borderBottomLeftRadius: theme.borderRadius.button,
+    backgroundColor: theme.colors.white,
+    flex: 0.5,
+    justifyContent: 'flex-end',
+  },
+  tabBarLabel: {
+    fontSize: 14,
+    lineHeight: 20,
+    letterSpacing: 0.1,
+    marginVertical: 8,
+    color: theme.colors.black,
+    alignItems: 'flex-end',
+  },
+  tabBarIndicatorStyle: {
+    backgroundColor: theme.colors.primary,
+    width: 100,
+    left: (Dimensions.get('window').width / 2 - 100) / 2,
+    height: 3,
+  },
+  logo: {
+    position: 'absolute',
+    top: Dimensions.get('window').height / 4 / 2,
+    alignSelf: 'center',
+  },
+});
 
 const Auth = ({
   navigation,
@@ -33,21 +60,21 @@ const Auth = ({
   const [index, setIndex] = useState(0);
 
   const [routes] = React.useState([
-    {key: 'first', title: 'Log In'},
-    {key: 'second', title: 'Sign Up'},
+    {key: 'login', title: 'Log In'},
+    {key: 'signup', title: 'Sign Up'},
   ]);
   return (
-    <SafeAreaView style={{backgroundColor: '#F2F2F2'}}>
+    <SafeAreaView style={styles.tabContainer}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <View style={{height: Dimensions.get('screen').height}}>
         <TabView
           navigationState={{index, routes}}
           renderScene={({route}) => {
             switch (route.key) {
-              case 'first':
+              case 'login':
                 return <Login />;
-              case 'second':
-                return <SecondRoute />;
+              case 'signup':
+                return <Signup />;
               default:
                 return null;
             }
@@ -55,33 +82,11 @@ const Auth = ({
           renderTabBar={props => (
             <TabBar
               {...props}
-              style={{
-                height: Dimensions.get('window').height / 3,
-                borderBottomRightRadius: theme.borderRadius.button,
-                borderBottomLeftRadius: theme.borderRadius.button,
-                backgroundColor: theme.colors.white,
-                flex: 0.5,
-                justifyContent: 'flex-end',
-              }}
+              style={styles.tabBarStyle}
               renderLabel={({route}) => (
-                <Text
-                  style={{
-                    fontSize: 14,
-                    lineHeight: 20,
-                    letterSpacing: 0.1,
-                    marginVertical: 8,
-                    color: theme.colors.black,
-                    alignItems: 'flex-end',
-                  }}>
-                  {route.title}
-                </Text>
+                <Text style={styles.tabBarLabel}>{route.title}</Text>
               )}
-              indicatorStyle={{
-                backgroundColor: theme.colors.primary,
-                width: 100,
-                left: (Dimensions.get('window').width / 2 - 100) / 2,
-                height: 3,
-              }}
+              indicatorStyle={styles.tabBarIndicatorStyle}
             />
           )}
           onIndexChange={setIndex}
@@ -89,12 +94,7 @@ const Auth = ({
         />
         <Image
           source={require('../../assets/Group3.png')}
-          style={{
-            position: 'absolute',
-            top: 100,
-            left: Dimensions.get('screen').width / 2,
-            right: Dimensions.get('screen').width /2,
-          }}
+          style={styles.logo}
         />
       </View>
     </SafeAreaView>
