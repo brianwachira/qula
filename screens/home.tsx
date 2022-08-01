8732fba8b0ae3e6bff70fe212ad60b10da799692
brianwachira <brianwachira7@gmail.com>
2022-08-01 16:06:36 +0300

    [feat] : mmkv storage

--- screens/home.tsx
+++ screens/home.tsx
@@ -1,6 +1,6 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
-import {StyleSheet,  View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import Text from '../components/shared-ui/text';
 import {RootStackParamList} from '../types';
 
