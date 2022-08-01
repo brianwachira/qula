6a2136558c67a18f55e62d48801c8bffcfc17a9a
brianwachira <brianwachira7@gmail.com>
2022-08-01 16:07:01 +0300

    [feat] : bottom navigation

--- navigation/bottomNavigation.tsx
+++ navigation/bottomNavigation.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from '../screens/home';
+import Liked from '../screens/liked';
+import History from '../screens/history';
+import Profile from '../screens/profile';
 
 const Tab = createBottomTabNavigator();
 
@@ -8,6 +11,9 @@ const BottomNavigation = () => {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Liked" component={Liked} />
+      <Tab.Screen name="History" component={History} />
+      <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
   );
 };
