decc57eab3eae154e110474d6e31126eb22c5144
brianwachira <brianwachira7@gmail.com>
2022-08-02 16:03:29 +0300

    [feat] : navigation separation of concerns feature

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import DrawerNavigator from './drawerNavigation';
 import {navigationRef} from './rootNavigation';
-import AuthNavigation from './authNavigation';
+import {useStorage} from '../hooks/useStorage';
+import Loading from '../components/shared-ui/loading';
+const AuthNavigation = React.lazy(() => import('./authNavigation'));
 
 const MainStackNavigator = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [user, setUser] = useStorage('email');
+
   return (
     <NavigationContainer ref={navigationRef}>
-      {/* <AuthNavigation /> */}
-      <DrawerNavigator />
+      {!user?.email ? (
+        <Suspense fallback={<Loading />}>
+          <AuthNavigation />
+        </Suspense>
+      ) : (
+        <DrawerNavigator />
+      )}
     </NavigationContainer>
   );
 };
