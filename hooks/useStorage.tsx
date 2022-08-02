d768c1abcb26de28051c93e5e672c4bc3f6d2e3f
brianwachira <brianwachira7@gmail.com>
2022-08-02 16:04:37 +0300

    [feat] : mmkvStorage hook for storage

--- hooks/useStorage.tsx
+++ hooks/useStorage.tsx
@@ -1,15 +1,21 @@
-import MMKVStorage, {
+import {
+  MMKVInstance,
   MMKVLoader,
   useMMKVStorage,
 } from 'react-native-mmkv-storage';
 
-const MMKV: MMKVStorage.API = new MMKVLoader().initialize();
+const MMKV: MMKVInstance = new MMKVLoader().initialize();
+
 type LiteralUnion<T extends U, U = string> = T | (U & {});
 
+type defaultObject = {
+  email: string;
+};
+
 export const useStorage = (
-  key: LiteralUnion<'user' | 'password'>,
-  defaultValue?: string,
+  key: LiteralUnion<'user' | 'email' | 'password'>,
+  defaultValue?: defaultObject,
 ) => {
   const [value, setValue] = useMMKVStorage(key, MMKV, defaultValue);
-  return [value, setValue];
+  return [value, setValue] as const;
 };
