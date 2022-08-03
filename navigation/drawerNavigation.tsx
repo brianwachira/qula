f5e2232d3ef4fc7b179afec1a775ebd8f7592e57
brianwachira <brianwachira7@gmail.com>
2022-08-03 15:15:11 +0300

    [feat] : drawerNavigation with bottom navigation

--- navigation/drawerNavigation.tsx
+++ navigation/drawerNavigation.tsx
@@ -1,26 +1,103 @@
 import React from 'react';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import {
+  createDrawerNavigator,
+  DrawerContentComponentProps,
+  DrawerContentScrollView,
+  DrawerItem,
+} from '@react-navigation/drawer';
 
 import BottomNavigation from './bottomNavigation';
-import Profile from '../screens/profile';
-import History from '../screens/history';
-import Liked from '../screens/liked';
-
+import {routes, screens} from './routes';
+import Text from '../components/shared-ui/text';
+import {StyleSheet} from 'react-native';
+import {NavigationContainerRef} from '@react-navigation/native';
 const Drawer = createDrawerNavigator();
 
-const DrawerNavigator = () => {
+type customDrawerContentProps = {
+  nav: () => NavigationContainerRef<ReactNavigation.RootParamList>;
+};
+
+const CustomDrawerContent = (
+  props: DrawerContentComponentProps & customDrawerContentProps,
+) => {
+  const currentRouteName = props.nav()?.getCurrentRoute()?.name;
   return (
-    <Drawer.Navigator>
+    <DrawerContentScrollView {...props}>
+      {routes
+        .filter(route => route.showInDrawer)
+        .map(route => {
+          const focusedRoute = routes.find(r => r.name === currentRouteName);
+          const focused = focusedRoute
+            ? route.name === focusedRoute?.focusedRoute
+            : route.name === screens.HomeStack;
+          return (
+            <DrawerItem
+              key={route.name}
+              label={() => (
+                <Text
+                  style={
+                    focused ? styles.drawerLabelFocused : styles.drawerLabel
+                  }>
+                  {route.title}
+                </Text>
+              )}
+              onPress={() => props.navigation.navigate(route.name)}
+              style={[
+                styles.drawerItem,
+                focused ? styles.drawerItemFocused : null,
+              ]}
+            />
+          );
+        })}
+    </DrawerContentScrollView>
+  );
+};
+
+const DrawerNavigator = ({
+  nav,
+}: {
+  nav: () => NavigationContainerRef<ReactNavigation.RootParamList>;
+}) => {
+  return (
+    <Drawer.Navigator
+      drawerContent={props => <CustomDrawerContent {...props} nav={nav} />}>
       <Drawer.Screen
-        name="HomeTabs"
+        name={screens.HomeTab}
         options={{title: 'Home'}}
         component={BottomNavigation}
       />
-      <Drawer.Screen name="Profile" component={Profile} />
-      <Drawer.Screen name="History" component={History} />
-      <Drawer.Screen name="Liked" component={Liked} />
     </Drawer.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  headerLeft: {
+    marginLeft: 15,
+  },
+  headerTitle: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  headerRight: {
+    marginRight: 15,
+  },
+  // drawer content
+  drawerLabel: {
+    fontSize: 14,
+  },
+  drawerLabelFocused: {
+    fontSize: 14,
+    color: '#551E18',
+    fontWeight: '500',
+  },
+  drawerItem: {
+    height: 50,
+    justifyContent: 'center',
+  },
+  drawerItemFocused: {
+    backgroundColor: '#ba9490',
+  },
+});
 
 export default DrawerNavigator;
