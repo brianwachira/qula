f560052ffd43057b7cc64cca5f552d8ca6d4a263
brianwachira <brianwachira7@gmail.com>
2022-08-03 15:16:18 +0300

    [feat] : bottomNavigation

--- navigation/bottomNavigation.tsx
+++ navigation/bottomNavigation.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from '../screens/home';
-import Liked from '../screens/liked';
-import History from '../screens/history';
 import Profile from '../screens/profile';
-import theme from '../styles/themes';
+import {routes, screens} from './routes';
+import {View} from 'react-native';
+import Text from '../components/shared-ui/text';
+import Orders from '../screens/orders';
+import Cart from '../screens/cart';
 
 const Tab = createBottomTabNavigator();
 
+const tabOptions = (props: {route: {name: string}}) => {
+  const item = routes.find(routeItem => routeItem.name === props.route.name);
+
+  if (!item?.showInTab) {
+    return {
+      tabBarButton: () => <View style={{width: 0}} />,
+      headerShown: false,
+      title: item?.title,
+    };
+  }
+
+  return {
+    tabBarIcon: (props2: {focused: any}) => item?.icon(props2.focused),
+    tabBarLabel: () => <Text>{item.title || ''}</Text>,
+    headerShown: false,
+    title: item.title,
+  };
+};
+
 const BottomNavigation = () => {
   return (
-    <Tab.Navigator
-      screenOptions={{
-        tabBarActiveTintColor: theme.colors.primary,
-        headerShown: false,
-      }}>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Liked" component={Liked} />
-      <Tab.Screen name="History" component={History} />
-      <Tab.Screen name="Profile" component={Profile} />
+    <Tab.Navigator screenOptions={tabOptions}>
+      <Tab.Screen name={screens.HomeStack} component={Home} />
+      <Tab.Screen name={screens.OrdersStack} component={Orders} />
+      <Tab.Screen name={screens.CartStack} component={Cart} />
+      <Tab.Screen name={screens.ProfileStack} component={Profile} />
     </Tab.Navigator>
   );
 };
