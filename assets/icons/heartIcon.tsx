dc5d81cae7f5857a0706d5e6721d7241367ce200
Brian Wachira <brianwachira7@gmail.com>
2022-08-05 12:55:23 +0300

    [feat] : fill prop to edit fill color on icon focus

--- assets/icons/heartIcon.tsx
+++ assets/icons/heartIcon.tsx
@@ -12,7 +12,7 @@ const HeartIcon = (props: SvgProps) => (
       fillRule="evenodd"
       clipRule="evenodd"
       d="M4.599 7.499a5.8 5.8 0 0 1 8.202 0L14.5 9.198l1.699-1.7a5.8 5.8 0 1 1 8.202 8.203L14.5 25.602l-9.901-9.9a5.8 5.8 0 0 1 0-8.203Z"
-      fill="#ADADAF"
+      fill={props.fill}
     />
   </Svg>
 );
