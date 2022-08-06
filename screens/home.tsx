8ac1c79208adaff7c359340fb9ed580202d5c012
Brian Wachira <brianwachira7@gmail.com>
2022-08-07 01:06:31 +0300

    [feat] : change status bar color and add FocusAwareStatusBar from react navigation

--- screens/home.tsx
+++ screens/home.tsx
@@ -1,8 +1,11 @@
+import {useDrawerStatus} from '@react-navigation/drawer';
+import {useIsFocused} from '@react-navigation/native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StatusBar, StyleSheet, View, StatusBarProps} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Text from '../components/shared-ui/text';
+import theme from '../styles/themes';
 import {RootStackParamList} from '../types';
 
 const styles = StyleSheet.create({
@@ -13,11 +16,28 @@ const styles = StyleSheet.create({
   },
 });
 
+const FocusAwareStatusBar = (
+  props: JSX.IntrinsicAttributes &
+    JSX.IntrinsicClassAttributes<StatusBar> &
+    Readonly<StatusBarProps>,
+) => {
+  const isFocused = useIsFocused();
+
+  return isFocused ? <StatusBar {...props} /> : null;
+};
+
 const Home = ({
   navigation,
 }: NativeStackScreenProps<RootStackParamList, 'Home'>) => {
+  const drawerStatus = useDrawerStatus();
   return (
-    <SafeAreaView style={{height: '100%', marginVertical: 40,}}>
+    <SafeAreaView style={{height: '100%', marginVertical: 40}}>
+      <FocusAwareStatusBar
+        barStyle={drawerStatus === 'open' ? 'light-content' : 'dark-content'}
+        backgroundColor={
+          drawerStatus === 'open' ? theme.colors.primary : theme.colors.tab
+        }
+      />
       <View style={styles.container}>
         <Text>Home</Text>
       </View>
