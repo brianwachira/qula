54bc572ad83604da7e56589dec5c07ed0e7989d0
Brian Wachira <brianwachira7@gmail.com>
2022-08-09 00:22:07 +0300

    [feat] : Empty state styles

--- screens/cart.tsx
+++ screens/cart.tsx
@@ -1,7 +1,10 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Dimensions, SafeAreaView, StyleSheet, View} from 'react-native';
+import CartIcon from '../assets/icons/cartIcon';
+import Button from '../components/shared-ui/button';
 import Text from '../components/shared-ui/text';
+import theme from '../styles/themes';
 import {RootStackParamList} from '../types';
 
 const styles = StyleSheet.create({
@@ -9,6 +12,25 @@ const styles = StyleSheet.create({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    marginTop: 70,
+    flex: 1,
+    marginHorizontal: 40,
+  },
+  contentContainer: {
+    flexGrow: 0.8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textOpacity: {
+    opacity: 0.57,
+    fontWeight: '400',
+    width: Dimensions.get('screen').width - 130,
+  },
+  marginBottomStyle: {
+    marginBottom: 30,
+  },
+  marginBottomStyle2: {
+    marginBottom: 20,
   },
 });
 
@@ -16,9 +38,30 @@ const Cart = ({
   navigation,
 }: NativeStackScreenProps<RootStackParamList, 'Cart'>) => {
   return (
-    <View style={styles.container}>
-      <Text>Cart</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.contentContainer}>
+        <CartIcon
+          style={styles.marginBottomStyle}
+          width={150}
+          height={150}
+          fill={theme.colors.icon}
+        />
+        <Text style={styles.marginBottomStyle2} textType="empty">
+          Nothing here yet
+        </Text>
+        <Text style={[styles.textOpacity]} textAlign="center">
+          Hit the orange button down below to view some orders
+        </Text>
+      </View>
+      <View>
+        <Button
+          title="Start Ordering"
+          buttonType="orange"
+          textType="labelButtonOrange"
+          accessibilityLabel="Start Ordering"
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
