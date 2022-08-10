350597178ecdb2cbe67b678d79f9770ee5995e52
Brian Wachira <brianwachira7@gmail.com>
2022-08-10 18:03:37 +0300

    [feat] : search bar

--- screens/home.tsx
+++ screens/home.tsx
@@ -1,10 +1,13 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
 import {FlatList, SafeAreaView, StyleSheet, View} from 'react-native';
+import SearchIcon from '../assets/icons/searchIcon';
 import CategoriesCard from '../components/categoriesCard';
 import RestuarantCard from '../components/restuarantCard.';
 import Text from '../components/shared-ui/text';
+import TextInput from '../components/shared-ui/textInput';
 import {categoriesData, localRestaurants} from '../mockdata';
+import theme from '../styles/themes';
 import {RootStackParamList} from '../types';
 
 const styles = StyleSheet.create({
@@ -23,6 +26,16 @@ const styles = StyleSheet.create({
   title: {
     marginBottom: 15,
   },
+  searchBar: {
+    backgroundColor: '#EFEEEE',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+    marginRight: 40,
+    borderRadius: 30,
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+  },
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
@@ -37,9 +50,16 @@ const Home = ({
           <Text textType="empty">Delicious</Text>
           <Text textType="empty">food for you</Text>
         </View>
-        {/* <View style={{height: 120}}>
-          <Text>Searchbar here</Text>
-        </View> */}
+        <View style={styles.searchBar}>
+          <SearchIcon />
+          <TextInput
+            style={{
+              color: theme.colors.black,
+            }}
+            placeholder="Search"
+            placeholderTextColor={theme.colors.black}
+          />
+        </View>
 
         <FlatList
           data={categoriesData}
