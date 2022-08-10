1acc0a5495a71f0481e3e3f3ad1c69650ee719ae
Brian Wachira <brianwachira7@gmail.com>
2022-08-10 16:54:02 +0300

    [feat] : categories container

---
+++ components/categoriesCard.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Image, StyleSheet, TouchableOpacity} from 'react-native';
+import theme from '../styles/themes';
+import Text from './shared-ui/text';
+
+const styles = StyleSheet.create({
+  categoriesContainer: {
+    backgroundColor: theme.colors.white,
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderRadius: 10,
+    marginRight: 15,
+  },
+  image: {
+    width: 50,
+    height: 50,
+  },
+});
+
+const CategoriesCard = ({onPress, item}: {onPress: () => void; item: any}) => {
+  return (
+    <TouchableOpacity style={styles.categoriesContainer} onPress={onPress}>
+      <Image style={styles.image} source={item.image} />
+      <Text textAlign="center">{item.text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CategoriesCard;
