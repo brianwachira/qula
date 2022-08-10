a5a46d8a69d675bc537fb0d04ae4b2751ee4521e
Brian Wachira <brianwachira7@gmail.com>
2022-08-10 16:55:33 +0300

    [feat] : restuarant card

---
+++ components/restuarantCard..tsx
@@ -0,0 +1,69 @@
+// resuarant card component
+
+import React from 'react';
+import {TouchableOpacity, View, Image, StyleSheet} from 'react-native';
+import theme from '../styles/themes';
+
+import Text from './shared-ui/text';
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 20,
+  },
+  image: {
+    borderRadius: 80,
+    height: 160,
+    width: 160,
+    resizeMode: 'cover',
+  },
+  cardContent: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 30,
+  },
+  timeWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  ratingWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#E7E6E2',
+    padding: 5,
+    borderRadius: 30,
+    marginTop: 10,
+  },
+  backdrop: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: theme.colors.white,
+    borderRadius: 20,
+    top: '20%',
+  },
+});
+
+const RestuarantCard = ({onPress, item}: {onPress: () => void; item: any}) => {
+  return (
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <View style={styles.backdrop} />
+      <Image
+        source={{
+          uri: item.image_url,
+        }}
+        style={styles.image}
+      />
+      <View style={styles.cardContent}>
+        <Text numberOfLines={1}>{item.name}</Text>
+        <View style={styles.timeWrapper}>
+          <Text> 20-30 • min • {item.price}</Text>
+        </View>
+        <View style={styles.ratingWrapper}>
+          <Text color="primary">{item.rating}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default RestuarantCard;
