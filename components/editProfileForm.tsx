9c882899fdbc99832fc8815fd9a05dd0a334d58f
brianwachira <brianwachira7@gmail.com>
2022-08-17 12:05:25 +0300

    [feat] : Edit profile form component

---
+++ components/editProfileForm.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import theme from '../styles/themes';
+import Button from './shared-ui/button';
+import FormikTextInput from './shared-ui/formikTextInput';
+import Text from './shared-ui/text';
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: theme.colors.white,
+    color: '#202731',
+    borderColor: '#818298',
+    borderWidth: 1,
+    height: 48,
+    borderRadius: 8,
+    padding: 10,
+  },
+});
+
+const EditProfileForm = ({onSubmit}: {onSubmit: any}) => {
+  return (
+    <View
+      style={{
+        flex: 0.7,
+        justifyContent: 'center',
+      }}>
+      <View
+        style={{
+          marginBottom: 20,
+        }}>
+        <Text textType="labelButtonWhite">Name</Text>
+        <FormikTextInput
+          name="name"
+          testID="name"
+          style={styles.input}
+          placeholderTextColor={theme.colors.primary}
+        />
+      </View>
+      <View
+        style={{
+          marginBottom: 20,
+        }}>
+        <Text textType="labelButtonWhite">Email Address</Text>
+        <FormikTextInput
+          name="email"
+          testID="email"
+          style={styles.input}
+          placeholderTextColor={theme.colors.primary}
+        />
+      </View>
+      <View
+        style={{
+          marginBottom: 10,
+        }}>
+        <Text textType="labelButtonWhite">Phone Number</Text>
+        <FormikTextInput
+          name="phone"
+          testID="phone"
+          style={styles.input}
+          placeholderTextColor={theme.colors.primary}
+        />
+      </View>
+      <View
+        style={{
+          marginBottom: 20,
+        }}
+      />
+      <Button
+        onPress={onSubmit}
+        title="Update Details"
+        accessibilityLabel="Manage your account"
+        testID="Update Details"
+      />
+    </View>
+  );
+};
+
+export default EditProfileForm;
