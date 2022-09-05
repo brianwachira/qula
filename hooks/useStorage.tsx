56eb0f51dc6e4a6aead5191f961d9ded9af3e18e
brianwachira <brianwachira7@gmail.com>
2022-09-05 15:24:48 +0300

    [feat] : have defaultStorageObject type

--- hooks/useStorage.tsx
+++ hooks/useStorage.tsx
@@ -3,24 +3,15 @@ import {
   MMKVLoader,
   useMMKVStorage,
 } from 'react-native-mmkv-storage';
-import {cartItem} from '../types/types';
+import {defaultStorageObject} from '../types/types';
 
 const MMKV: MMKVInstance = new MMKVLoader().initialize();
 
 type LiteralUnion<T extends U, U = string> = T | (U & {});
 
-type defaultObject = {
-  email?: string;
-  authKey: string;
-  phone: string;
-  userId: string;
-  clientId: string;
-  products: cartItem[] | [];
-};
-
 export const useStorage = (
   key: LiteralUnion<'user' | 'email' | 'password'>,
-  defaultValue?: defaultObject,
+  defaultValue?: defaultStorageObject,
 ) => {
   const [value, setValue] = useMMKVStorage(key, MMKV, defaultValue);
   return [value, setValue] as const;
