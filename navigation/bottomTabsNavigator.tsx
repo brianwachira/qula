396b7e6eb36b1fbeffd92afe7ac1a132cab1a17a
brianwachira <brianwachira7@gmail.com>
2022-10-06 19:28:39 +0300

    [feat] : edit cart screen

--- navigation/bottomTabsNavigator.tsx
+++ navigation/bottomTabsNavigator.tsx
@@ -16,99 +16,95 @@ import RefreshIcon from '../assets/icons/refreshIcon';
 import CartIcon from '../assets/icons/cartIcon';
 import UserCircleIcon from '../assets/icons/userCircleIcon';
 import {StyleSheet} from 'react-native';
+import FocusAwareStatusBar from '../components/shared-ui/focusAwareStatusBar';
 
 const BottomTabsNavigator = () => {
   return (
-    <Tab.Navigator
-      screenOptions={({}) => ({
-        headerShown: false,
-        tabBarActiveTintColor: theme.colors.primary,
-        tabBarInactiveColor: theme.colors.tab,
-        headerTitleAlign: 'center',
-        headerTitleStyle: {
-          marginTop: 35,
-          fonstSize: theme.fontSizes.label.normal.fontSize,
-          lineHeight: theme.fontSizes.label.normal.lineHeight,
-        },
-        headerTransparent: true,
-        tabBarStyle: styles.tabStyleWithContent,
-        lazy: false,
-        tabBarLabelStyle: {
-          ...theme.fontSizes.label.small,
-        },
-      })}>
-      <Tab.Screen
-        name={screens.Home}
-        component={Home}
-        options={{
-          tabBarIcon: (props: {
-            focused: boolean;
-            color: string;
-            size: number;
-          }) => (
-            <HomeIcon
-              fill={props.focused ? theme.colors.primary : theme.colors.icon}
-            />
-          ),
-        }}
+    <>
+      <FocusAwareStatusBar
+        barStyle={'dark-content'}
+        backgroundColor={theme.colors.tab}
       />
-      <Tab.Screen
-        name={screens.Orders}
-        component={Orders}
-        options={{
-          tabBarIcon: (props: {
-            focused: boolean;
-            color: string;
-            size: number;
-          }) => (
-            <RefreshIcon
-              fill={props.focused ? theme.colors.primary : theme.colors.icon}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name={screens.Cart}
-        component={Cart}
-        options={{
-          tabBarIcon: (props: {
-            focused: boolean;
-            color: string;
-            size: number;
-          }) => (
-            <CartIcon
-              fill={props.focused ? theme.colors.primary : theme.colors.icon}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name={screens.Profile}
-        component={Profile}
-        options={{
-          tabBarIcon: (props: {
-            focused: boolean;
-            color: string;
-            size: number;
-          }) => (
-            <UserCircleIcon
-              fill={props.focused ? theme.colors.primary : theme.colors.icon}
-            />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({}) => ({
+          headerShown: false,
+          tabBarActiveTintColor: theme.colors.primary,
+          tabBarInactiveColor: theme.colors.tab,
+          tabBarStyle: styles.tabStyleWithContent,
+          lazy: false,
+          tabBarLabelStyle: {
+            ...theme.fontSizes.label.small,
+          },
+        })}>
+        <Tab.Screen
+          name={screens.Home}
+          component={Home}
+          options={{
+            tabBarIcon: (props: {
+              focused: boolean;
+              color: string;
+              size: number;
+            }) => (
+              <HomeIcon
+                fill={props.focused ? theme.colors.primary : theme.colors.icon}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={screens.Orders}
+          component={Orders}
+          options={{
+            tabBarIcon: (props: {
+              focused: boolean;
+              color: string;
+              size: number;
+            }) => (
+              <RefreshIcon
+                fill={props.focused ? theme.colors.primary : theme.colors.icon}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={screens.Cart}
+          component={Cart}
+          options={{
+            tabBarIcon: (props: {
+              focused: boolean;
+              color: string;
+              size: number;
+            }) => (
+              <CartIcon
+                fill={props.focused ? theme.colors.primary : theme.colors.icon}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={screens.Profile}
+          component={Profile}
+          options={{
+            tabBarIcon: (props: {
+              focused: boolean;
+              color: string;
+              size: number;
+            }) => (
+              <UserCircleIcon
+                fill={props.focused ? theme.colors.primary : theme.colors.icon}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   tabStyleWithContent: {
     backgroundColor: theme.colors.tab,
-    borderTop: 0,
-    borderTopWidth: 0,
-    elevation: 0,
-    paddingVertical: 0,
-    marginVertical: 5,
+    paddingBottom: 5,
     alignItems: 'center',
     flex: 0.07,
   },
