852ad413b35beb50ea2653de0792bd0a53c4ba8d
brianwachira <brianwachira7@gmail.com>
2022-10-06 12:13:15 +0300

    [refactor] : remove drawer navigation and place screens in correct area

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,20 +1,31 @@
+// main navigation
 import React, {Suspense} from 'react';
-import {
-  NavigationContainer,
-  NavigationContainerRef,
-} from '@react-navigation/native';
-import DrawerNavigator from './drawerNavigation';
-import {navigationRef} from './rootNavigation';
-import {useStorage} from '../hooks/useStorage';
-import Loading from '../components/shared-ui/loading';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+// Auth Navigation
 const AuthNavigation = React.lazy(() => import('./authNavigation'));
+
+// Create stack.
+const Stack = createNativeStackNavigator();
+
+// Import screens.
+import BottomTabsNavigator from './bottomTabsNavigator';
+import {NavigationContainer} from '@react-navigation/native';
+import {screens} from './routes';
+import {useStorage} from '../hooks/useStorage';
+import {navigationRef} from './rootNavigation';
+import Loading from '../components/shared-ui/loading';
+import SearchResults from '../screens/searchResults';
+import RestuarantDetails from '../screens/restuarantDetails';
+import FoodDetails from '../screens/foodDetails';
+import OrderDetails from '../screens/orderDetails';
+import Checkout from '../screens/checkout';
+import EditProfile from '../screens/profile/editProfile';
 
 const MainStackNavigator = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [user, setUser] = useStorage('user');
 
-  const nav: () => NavigationContainerRef<ReactNavigation.RootParamList> = () =>
-    navigationRef.current as NavigationContainerRef<ReactNavigation.RootParamList>;
   return (
     <NavigationContainer ref={navigationRef}>
       {!user ? (
@@ -22,7 +33,24 @@ const MainStackNavigator = () => {
           <AuthNavigation />
         </Suspense>
       ) : (
-        <DrawerNavigator nav={nav} />
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen
+            name={screens.HomeTab}
+            component={BottomTabsNavigator}
+          />
+          <Stack.Screen
+            name={screens.SearchResults}
+            component={SearchResults}
+          />
+          <Stack.Screen
+            name={screens.RestuarantDetails}
+            component={RestuarantDetails}
+          />
+          <Stack.Screen name={screens.FoodDetails} component={FoodDetails} />
+          <Stack.Screen name={screens.OrderDetails} component={OrderDetails} />
+          <Stack.Screen name={screens.Checkout} component={Checkout} />
+          <Stack.Screen name={screens.EditProfile} component={EditProfile} />
+        </Stack.Navigator>
       )}
     </NavigationContainer>
   );
