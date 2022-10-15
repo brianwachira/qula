9fa50c589476490900e81b24fb1e1c10fc3ad393
brianwachira <brianwachira7@gmail.com>
2022-10-15 11:22:44 +0300

    [feat] : loading component

--- components/shared-ui/loading.tsx
+++ components/shared-ui/loading.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import {View, ActivityIndicator, StyleSheet} from 'react-native';
+import theme from '../../styles/themes';
 
 const styles = StyleSheet.create({
   loadingContainer: {
     flex: 1,
     justifyContent: 'center',
+    backgroundColor: theme.colors.primary,
   },
 });
 
 const Loading = () => {
   return (
     <View style={styles.loadingContainer}>
-      <ActivityIndicator color="#000" animating size="small" />
+      <ActivityIndicator color={theme.colors.black} animating size="small" />
     </View>
   );
 };
