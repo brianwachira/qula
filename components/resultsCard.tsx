fa9b8c07e1bc0d447048a0c9a9a36e81ed0246eb
Brian Wachira <brianwachira7@gmail.com>
2022-10-16 00:49:22 +0300

    [feat] : add image base url for rendering purposes

--- components/resultsCard.tsx
+++ components/resultsCard.tsx
@@ -1,4 +1,5 @@
 // results card
+import {IMAGE_BASE_URL} from '@env';
 import React from 'react';
 import {
   TouchableOpacity,
@@ -29,7 +30,7 @@ const ResultsCard = ({
       <View style={styles.backdrop} />
       <Image
         source={{
-          uri: item.image_path,
+          uri: `${IMAGE_BASE_URL}/${item.image_path}`,
         }}
         style={styles.image}
       />
