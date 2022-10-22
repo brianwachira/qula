2945eb5ebb62f17c87562a73b6704f2ae3d5f7b7
brianwachira <brianwachira7@gmail.com>
2022-10-22 11:47:45 +0300

    [feat] : add location dropdown

--- screens/searchResults.tsx
+++ screens/searchResults.tsx
@@ -1,4 +1,7 @@
+import {API_URL} from '@env';
+import {Picker} from '@react-native-picker/picker';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import axios from 'axios';
 import React, {useEffect, useRef, useState} from 'react';
 import {
   Dimensions,
@@ -66,6 +69,15 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   backIcon: {marginRight: 10},
+  pickerStyle: {
+    width: 190,
+    maxHeight: 50,
+    color: 'black',
+    marginLeft: -80,
+    marginRight: -180,
+    marginVertical: -5,
+    fontSize: 12,
+  },
 });
 const ItemSeparator = () => <View style={styles.separator} />;
 
@@ -74,6 +86,8 @@ const SearchResults = ({
   navigation,
 }: NativeStackScreenProps<RootStackParamList, 'SearchResults'>) => {
   const [searchedMerchants, setSearchedMerchants] = useState<Imerchants[]>();
+  const [locations, setLocations] = useState<string[]>([]);
+  const [selectedLocation, setSelectedLocation] = useState<string>('');
 
   const {token, clientId, merchants} = route.params;
   // create a ref for search bar
@@ -82,6 +96,19 @@ const SearchResults = ({
   useEffect(() => {
     // focus on search bar on screen enter
     searchBarRef?.current?.focus();
+    axios
+      .get(`${API_URL}/get-locations`)
+      .then(response => {
+        if ((response.data.status = false)) {
+          console.log(response.data);
+        } else {
+          console.log(response.data.data);
+          setLocations(response.data.data);
+        }
+      })
+      .catch(error => {
+        console.log(error.response.data);
+      });
   }, []);
 
   // handle change function
@@ -94,7 +121,12 @@ const SearchResults = ({
       );
     }
   }
-
+  const handleChangeLocation = (text: string) => {
+    setSelectedLocation(text);
+    setSearchedMerchants(
+      merchants.filter(merchant => merchant.location === text),
+    );
+  };
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar
@@ -109,7 +141,7 @@ const SearchResults = ({
         <TextInput
           style={{
             color: theme.colors.black,
-            width: Dimensions.get('screen').width - 100,
+            width: Dimensions.get('screen').width / 2,
           }}
           selectionColor={theme.colors.black}
           placeholder="Search"
@@ -117,6 +149,23 @@ const SearchResults = ({
           ref={searchBarRef}
           onChangeText={text => onChangeText(text)}
         />
+        <View>
+          <Picker
+            selectedValue={selectedLocation}
+            onValueChange={(itemValue: string) =>
+              handleChangeLocation(itemValue)
+            }
+            style={[[styles.pickerStyle]]}
+            dropdownIconColor={theme.colors.grey}>
+            {locations.map(locationItem => (
+              <Picker.Item
+                key={locationItem}
+                label={locationItem}
+                value={locationItem}
+              />
+            ))}
+          </Picker>
+        </View>
       </View>
       {!searchedMerchants || searchedMerchants.length < 1 ? (
         <View style={styles.cardsContainer}>
